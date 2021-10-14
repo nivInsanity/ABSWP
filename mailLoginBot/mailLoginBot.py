c5788aa58dc129edfea766ee8f544ad78788e514
Nivisnowflake <48964180+Nivisnowflake@users.noreply.github.com>
2021-10-14 23:59:45 +0200

    wpisałem todo

---
+++ mailLoginBot/mailLoginBot.py
@@ -0,0 +1,11 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+
+print('Za warudo')
+
+#TODO: Zrobić moduł, który pobiera stronę za pomocą modułu BeautifulSoup, analizuje kod, szuka stringów odpowiedzialnych
+#TODO: za wprowadzanie danych, po tym podmienia kod na dane do logowania na protonmail i klika przycisk logowania
+
