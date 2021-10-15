315a09236a9bfd15e25974984c8fd6dd5b0d3449
Nivis <barzie5554@gmail.com>
2021-10-15 20:39:02 +0200

    'requests jedzo gówno'

--- mailLoginBot/mailLoginBot.py
+++ mailLoginBot/mailLoginBot.py
@@ -6,6 +6,11 @@ from selenium import webdriver
 
 print('Za warudo')
 
+driver = webdriver.Firefox()
+
+driver.get('https://protonmail.com/pl/')
+
 #TODO: Zrobić moduł, który pobiera stronę za pomocą modułu BeautifulSoup, analizuje kod, szuka stringów odpowiedzialnych
 #TODO: za wprowadzanie danych, po tym podmienia kod na dane do logowania na protonmail i klika przycisk logowania
 
+#Request nie daje sobie z tym rady - pobiera tylko html, a potrzebujemy elementów JS, trzeba spróbować pobrać html za pomocą selenium
